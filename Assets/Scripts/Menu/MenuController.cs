9edb6c3027da742e1460f2689ae0ed297d7d13af
Fred Gohsman <fred.gohsman@agilethought.com>
2021-01-31 15:54:36 -0500

    added quit button

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
